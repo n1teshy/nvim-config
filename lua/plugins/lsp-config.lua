4f2d2d31d8f6bf7c9bd50e731bdbf7f06fa91f61
Nitesh Yadav <nitesh.txt@gmail.com>
2025-12-28 17:19:55 +0530

    complete, stable

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,29 +1,33 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "pylsp"}
-      })
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      vim.lsp.config("lua_ls", {})
-      vim.lsp.enable("lua_ls")
-      vim.lsp.config("pylsp", {})
-      vim.lsp.enable("pylsp")
-    end
-  }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pylsp" },
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			vim.lsp.config("lua_ls", {
+				capabilities = capabilities,
+			})
+			vim.lsp.enable("lua_ls")
+			vim.lsp.config("pylsp", {
+				capabilities = capabilities,
+			})
+			vim.lsp.enable("pylsp")
+		end,
+	},
 }
-
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   lazy = false,
   build = ":TSUpdate",
   opts = {
-    ensure_installed = {"lua", "python", "javascript" },
+    auto_install = true,
     highlight = { enable = true },
     indent = { enable = true },
   },
